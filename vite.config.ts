74ccefcab3f182aa8136007cca08c36dd1c30b4d
Dmitry Chankov <chankovbelka123@yandex.ru>
2025-01-01 23:08:18 +0400

    Добавить начальную настройку проекта с использованием Vite и React, включая конфигурацию ESLint и Prettier, а также базовые стили и структуру папок.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': './src',
+      '@utils': './src/utils',
+      '@hooks': './src/hooks',
+      '@components': './src/components',
+      '@pages': './src/pages'
+    }
+  }
 })
